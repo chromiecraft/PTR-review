f370fcfcf56c257bef1ec9eb0405ad5dfcba4114
Dan <83884799+elthehablo@users.noreply.github.com>
2024-09-22 09:59:24 +0200

    Create black_temple_tuning_mythic.sql

---
+++ current_raid_tuning/black_temple_tuning_mythic.sql
@@ -0,0 +1,136 @@
+-- CURRENTLY WIP
+-- High Warlord Naj'entus
+UPDATE `creature_template` SET `HealthModifier`=3.5*350 WHERE `entry`=22887; -- High Warlord Naj'entus
+
+-- Supremus
+UPDATE `creature_template` SET `HealthModifier`=3.5*420 WHERE `entry`=22898; -- Supremus
+
+-- Shade of Akama
+UPDATE `creature_template` SET `HealthModifier`=3.5*92.4 WHERE `entry`=22841; -- Shade of Akama
+UPDATE `creature_template` SET `HealthModifier`=3.5*165 WHERE `entry`=23191; -- Akama
+UPDATE `creature_template` SET `HealthModifier`=3*17.5 WHERE `entry`=23421; -- Ashtongue Channeler
+UPDATE `creature_template` SET `HealthModifier`=3*14 WHERE `entry`=23215; -- Ashtongue Sorcerer
+UPDATE `creature_template` SET `HealthModifier`=3*8.4 WHERE `entry`=23216; -- Ashtongue Defender
+UPDATE `creature_template` SET `HealthModifier`=3*2.8 WHERE `entry`=23318; -- Ashtongue Rogue
+UPDATE `creature_template` SET `HealthModifier`=3*2.8 WHERE `entry`=23523; -- Ashtongue Elementalist
+UPDATE `creature_template` SET `HealthModifier`=3*2.8 WHERE `entry`=23524; -- Ashtongue Spiritbinder
+
+-- Teron Gorefiend
+UPDATE `creature_template` SET `HealthModifier`=3.5*577.5 WHERE `entry`=22871; -- Teron Gorefiend
+UPDATE `creature_template` SET `HealthModifier`=3*0.7 WHERE `entry`=23123; -- Doom Blossom
+
+-- Gurtogg Bloodboil
+UPDATE `creature_template` SET `HealthModifier`=3.5*525 WHERE `entry`=22948; -- Gurtogg Bloodboil
+
+-- Reliquary of the Lost
+UPDATE `creature_template` SET `HealthModifier`=3.5*700 WHERE `entry`=22856; -- Reliquary of the Lost
+UPDATE `creature_template` SET `HealthModifier`=3.5*210 WHERE `entry`=23418; -- Essence of Suffering
+UPDATE `creature_template` SET `HealthModifier`=3.5*280 WHERE `entry`=23419; -- Essence of Desire
+UPDATE `creature_template` SET `HealthModifier`=3.5*280 WHERE `entry`=23420; -- Essence of Anger
+UPDATE `creature_template` SET `HealthModifier`=3*1.4 WHERE `entry`=23469; -- Enslaved Soul
+
+-- Mother Shahraz
+UPDATE `creature_template` SET `HealthModifier`=3.5*525 WHERE `entry`=22947; -- Mother Shahraz
+
+-- Illidari Council
+UPDATE `creature_template` SET `HealthModifier`=3.5*175 WHERE `entry`=22949; -- Gathios the Shatterer
+UPDATE `creature_template` SET `HealthModifier`=3.5*175 WHERE `entry`=22950; -- High Nethermancer Zerevor
+UPDATE `creature_template` SET `HealthModifier`=3.5*175 WHERE `entry`=22951; -- Lady Malande
+UPDATE `creature_template` SET `HealthModifier`=3.5*175 WHERE `entry`=22952; -- Veras Darkshadow
+UPDATE `creature_template` SET `HealthModifier`=3.5*700 WHERE `entry`=23426; -- The Illidari Council
+
+-- Illidan
+UPDATE `creature_template` SET `HealthModifier`=3.5*50 WHERE `entry`=23089; -- Akama
+UPDATE `creature_template` SET `HealthModifier`=3.2*560 WHERE `entry`=22917; -- Illidan Stormrage
+UPDATE `creature_template` SET `HealthModifier`=3.5*2.1 WHERE `entry`=23375; -- Shadow Demon
+UPDATE `creature_template` SET `HealthModifier`=3*10.5 WHERE `entry`=23197; -- Maiev Shadowsong
+
+-- Trash and misc
+UPDATE `creature_template` SET `HealthModifier`=1.5*31.5 WHERE `entry`=22844; -- Ashtongue Battlelord
+UPDATE `creature_template` SET `HealthModifier`=1.5*21 WHERE `entry`=22845; -- Ashtongue Mystic
+UPDATE `creature_template` SET `HealthModifier`=1.5*21 WHERE `entry`=22846; -- Ashtongue Stormcaller
+UPDATE `creature_template` SET `HealthModifier`=1.5*21 WHERE `entry`=22847; -- Ashtongue Primalist
+UPDATE `creature_template` SET `HealthModifier`=1.5*10.5 WHERE `entry`=22848; -- Storm Fury
+UPDATE `creature_template` SET `HealthModifier`=1.5*10.5 WHERE `entry`=22849; -- Ashtongue Feral Spirit
+UPDATE `creature_template` SET `HealthModifier`=1.5*21 WHERE `entry`=22853; -- Illidari Defiler
+UPDATE `creature_template` SET `HealthModifier`=1.5*42 WHERE `entry`=22855; -- Illidari Nightlord
+UPDATE `creature_template` SET `HealthModifier`=1.5*21 WHERE `entry`=22869; -- Illidari Boneslicer
+UPDATE `creature_template` SET `HealthModifier`=1.5*28 WHERE `entry`=22873; -- Coilskar General
+UPDATE `creature_template` SET `HealthModifier`=1.5*21 WHERE `entry`=22874; -- Coilskar Harpooner
+UPDATE `creature_template` SET `HealthModifier`=1.5*21 WHERE `entry`=22875; -- Coilskar Sea-Caller
+UPDATE `creature_template` SET `HealthModifier`=1.5*21 WHERE `entry`=22876; -- Coilskar Soothsayer
+UPDATE `creature_template` SET `HealthModifier`=1.5*21 WHERE `entry`=22877; -- Coilskar Wrangler
+UPDATE `creature_template` SET `HealthModifier`=1.5*42 WHERE `entry`=22878; -- Aqueous Lord
+UPDATE `creature_template` SET `HealthModifier`=1.5*21 WHERE `entry`=22879; -- Shadowmoon Reaver
+UPDATE `creature_template` SET `HealthModifier`=1.5*31.5 WHERE `entry`=22880; -- Shadowmoon Champion
+UPDATE `creature_template` SET `HealthModifier`=1.5*10.5 WHERE `entry`=22881; -- Aqueous Surger
+UPDATE `creature_template` SET `HealthModifier`=1.5*21 WHERE `entry`=22882; -- Shadowmoon Deathshaper
+UPDATE `creature_template` SET `HealthModifier`=1.5*6.3 WHERE `entry`=22883; -- Aqueous Spawn
+UPDATE `creature_template` SET `HealthModifier`=1.5*52.5 WHERE `entry`=22884; -- Leviathan
+UPDATE `creature_template` SET `HealthModifier`=1.5*10.5 WHERE `entry`=22885; -- Dragon Turtle
+UPDATE `creature_template` SET `HealthModifier`=1.5*1.4 WHERE `entry`=22886; -- Black Temple Captive
+UPDATE `creature_template` SET `HealthModifier`=1.5*3.15 WHERE `entry`=22939; -- Temple Concubine
+UPDATE `creature_template` SET `HealthModifier`=1.5*17.5 WHERE `entry`=22945; -- Shadowmoon Blood Mage
+UPDATE `creature_template` SET `HealthModifier`=1.5*7 WHERE `entry`=22946; -- Shadowmoon War Hound
+UPDATE `creature_template` SET `HealthModifier`=1.5*35 WHERE `entry`=22953; -- Wrathbone Flayer
+UPDATE `creature_template` SET `HealthModifier`=1.5*52.5 WHERE `entry`=22954; -- Illidari Fearbringer
+UPDATE `creature_template` SET `HealthModifier`=1.5*3.15 WHERE `entry`=22955; -- Charming Courtesan
+UPDATE `creature_template` SET `HealthModifier`=1.5*35 WHERE `entry`=22956; -- Sister of Pain
+UPDATE `creature_template` SET `HealthModifier`=1.5*63 WHERE `entry`=22957; -- Priestess of Dementia
+UPDATE `creature_template` SET `HealthModifier`=1.5*21 WHERE `entry`=22959; -- Spellbound Attendant
+UPDATE `creature_template` SET `HealthModifier`=1.5*24.5 WHERE `entry`=22960; -- Dragonmaw Wyrmcaller
+UPDATE `creature_template` SET `HealthModifier`=1.5*63 WHERE `entry`=22962; -- Priestess of Delight
+UPDATE `creature_template` SET `HealthModifier`=1.5*3.15 WHERE `entry`=22963; -- Bonechewer Worker
+UPDATE `creature_template` SET `HealthModifier`=1.5*35 WHERE `entry`=22964; -- Sister of Pleasure
+UPDATE `creature_template` SET `HealthModifier`=1.5*17.5 WHERE `entry`=22965; -- Enslaved Servant
+UPDATE `creature_template` SET `HealthModifier`=1.5*24.5 WHERE `entry`=23018; -- Shadowmoon Houndmaster
+UPDATE `creature_template` SET `HealthModifier`=1.5*21 WHERE `entry`=23028; -- Bonechewer Taskmaster
+UPDATE `creature_template` SET `HealthModifier`=1.5*24.5 WHERE `entry`=23030; -- Dragonmaw Sky Stalker
+UPDATE `creature_template` SET `HealthModifier`=1.5*3.5 WHERE `entry`=23047; -- Shadowmoon Soldier
+UPDATE `creature_template` SET `HealthModifier`=1.5*35 WHERE `entry`=23049; -- Shadowmoon Weapon Master
+UPDATE `creature_template` SET `HealthModifier`=1.5*0.2 WHERE `entry`=23087; -- Sewer Crocolisk
+UPDATE `creature_template` SET `HealthModifier`=1.5*3.15 WHERE `entry`=23147; -- Shadowmoon Grunt
+UPDATE `creature_template` SET `HealthModifier`=1.5*4.9 WHERE `entry`=23157; -- Aluyen
+UPDATE `creature_template` SET `HealthModifier`=1.5*4.9 WHERE `entry`=23158; -- Seer Kanai
+UPDATE `creature_template` SET `HealthModifier`=1.5*4.9 WHERE `entry`=23159; -- Okuno
+UPDATE `creature_template` SET `HealthModifier`=1.5*24.5 WHERE `entry`=23172; -- Hand of Gorefiend
+UPDATE `creature_template` SET `HealthModifier`=1.5*63 WHERE `entry`=23196; -- Bonechewer Behemoth
+UPDATE `creature_template` SET `HealthModifier`=1.5*38.5 WHERE `entry`=23222; -- Bonechewer Brawler
+UPDATE `creature_template` SET `HealthModifier`=1.5*3.15 WHERE `entry`=23223; -- Bonechewer Spectator
+UPDATE `creature_template` SET `HealthModifier`=1.5*14 WHERE `entry`=23232; -- Mutant War Hound
+UPDATE `creature_template` SET `HealthModifier`=1.5*21 WHERE `entry`=23235; -- Bonechewer Blade Fury
+UPDATE `creature_template` SET `HealthModifier`=1.5*21 WHERE `entry`=23236; -- Bonechewer Shield Disciple
+UPDATE `creature_template` SET `HealthModifier`=1.5*21 WHERE `entry`=23237; -- Bonechewer Blood Prophet
+UPDATE `creature_template` SET `HealthModifier`=1.5*38.5 WHERE `entry`=23239; -- Bonechewer Combatant
+UPDATE `creature_template` SET `HealthModifier`=1.5*24.5 WHERE `entry`=23330; -- Dragonmaw Wind Reaver
+UPDATE `creature_template` SET `HealthModifier`=1.5*28 WHERE `entry`=23337; -- Illidari Centurion
+UPDATE `creature_template` SET `HealthModifier`=1.5*21 WHERE `entry`=23339; -- Illidari Heartseeker
+UPDATE `creature_template` SET `HealthModifier`=1.5*21 WHERE `entry`=23374; -- Ashtongue Stalker
+UPDATE `creature_template` SET `HealthModifier`=1.5*63 WHERE `entry`=23394; -- Promenade Sentinel
+UPDATE `creature_template` SET `HealthModifier`=1.5*28 WHERE `entry`=23397; -- Illidari Blood Lord
+UPDATE `creature_template` SET `HealthModifier`=1.5*0.7 WHERE `entry`=23398; -- Angered Soul Fragment
+UPDATE `creature_template` SET `HealthModifier`=1.5*8.4 WHERE `entry`=23399; -- Suffering Soul Fragment
+UPDATE `creature_template` SET `HealthModifier`=1.5*21 WHERE `entry`=23400; -- Illidari Archon
+UPDATE `creature_template` SET `HealthModifier`=1.5*4.2 WHERE `entry`=23401; -- Hungering Soul Fragment
+UPDATE `creature_template` SET `HealthModifier`=1.5*21 WHERE `entry`=23402; -- Illidari Battle-mage
+UPDATE `creature_template` SET `HealthModifier`=1.5*17.5 WHERE `entry`=23403; -- Illidari Assassin
+UPDATE `creature_template` SET `HealthModifier`=1.5*0.91 WHERE `entry`=23410; -- Spirit of Udalo
+UPDATE `creature_template` SET `HealthModifier`=1.5*0.91 WHERE `entry`=23411; -- Spirit of Olum
+
+-- Overall buffs
+DELETE FROM `zone_difficulty_info` WHERE `MapID`= 564;
+INSERT INTO `zone_difficulty_info` (`MapID`, `PhaseMask`, `HealingNerfValue`, `AbsorbNerfValue`, `MeleeDmgBuffValue`, `SpellDmgBuffValue`, `Enabled`, `Comment`) VALUES
+(564, 0, 0.75, 0.75, 1.43, 1.32, 1, 'The Black Temple');
+
+-- overriden and nerfed spells thanks to Jedbouk mostly
+DELETE FROM `zone_difficulty_spelloverrides` WHERE `SpellId` IN (39878, 40314, 40175, 40175, 41303, 41360, 40827, 40871, 42005);
+INSERT INTO `zone_difficulty_spelloverrides` (`SpellID`, `MapId`, `NerfValue`, `Enabled`, `Comment`) VALUES
+(39878, 564, 1, 1, 'Black Temple - Najentus - Tidal Burst Effect'),
+(40314, 564, 1, 1, 'Black Temple - Vengeful Spirit - Spirit Volley'),
+(40175, 564, 1, 1, 'Black Temple - Vengeful Spirit - Spirit Chains'),
+(40157, 564, 1, 1, 'Black Temple - Vengeful Spirit - Spirit Lance'),
+(41303, 564, 1, 1, 'Black Temple - Reliquary of the Lost - Essence of Suffering - Soul Drain'),
+(41360, 564, 1, 1, 'Black Temple - Promenade Sentinel - Arcane charge'), -- may not work
+(40827, 564, 1, 1, 'Black Temple - Mother Shahraz - Sinful Beam'),
+(40871, 564, 1, 1, 'Black Temple - Mother Shahraz - Fatal Attraction'),
+(42005, 564, 1, 1, 'Black Temple - Gurtogg Bloodboil - Bloodboil');
